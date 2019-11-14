53321f309f82071b4099646250b837ae34225b5c
Ingrid sura <sura.ingrid@gmail.com>
2019-11-14 04:07:32 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,12 @@ def my_all?(collection)
  i = 0
 block_returns_values = []
  while i < collection.length 
- clock_return_values << yield(collection[i])
+ block_return_values << yield(collection[i])
  i= i + 1
- end 
 end
+if block_return_values.include?(false)
+    false
+  else
+     true 
+   end
+end 
